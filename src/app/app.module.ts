58ea72ab4ce3f3e12aded3cb00ee4cf18cb6d668
apip <apip@gitlab.lab>
2020-10-29 09:02:28 +0700

    rename folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 
-import { SharedModule } from '../app/shared/shared-module.module';
+import { SharedModule } from './shared/shared.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { PrimeNgModule } from './prime-Ng.module';
+import { PrimeNgModule } from './prime-ng.module';
 
 @NgModule({
     imports: [],
