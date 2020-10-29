dd75e6bb2e78abcbdb61a9316bb3c2674e0ae8fe
apip <apip@gitlab.lab>
2020-10-29 08:49:11 +0700

    import component primeng

--- src/app/shared/prime-Ng.module.ts
+++ src/app/shared/prime-Ng.module.ts
@@ -1,24 +1,27 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 import { InputTextModule } from 'primeng/inputtext';
 import { PanelMenuModule } from 'primeng/panelmenu';
+import { MessagesModule } from 'primeng/messages';
+import { MessageModule } from 'primeng/message';
+import { ToastModule } from 'primeng/toast';
+import { CarouselModule } from 'primeng/carousel';
+import { GalleriaModule } from 'primeng/galleria';
 
 @NgModule({
-    imports: [
-        CommonModule,
-        ButtonModule,
-        CardModule,
-        InputTextModule,
-        PanelMenuModule
-    ],
+    imports: [],
     exports: [
         ButtonModule,
         CardModule,
         InputTextModule,
-        PanelMenuModule
+        PanelMenuModule,
+        MessagesModule,
+        MessageModule,
+        CarouselModule,
+        GalleriaModule,
+        ToastModule
     ],
     declarations: []
 })
