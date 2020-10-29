b513f3940c595caf5c3195218feedfa1d6d707e1
apip <apip@gitlab.lab>
2020-10-29 13:12:37 +0700

    create dashboard

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
+  itemCard: any[];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.itemCard = [
+      { id: '1', title: 'Covid Tracker', image: '', },
+      { id: '2', title: 'Calculator', image: '', },
+      { id: '3', title: 'Timer', image: '', },
+      { id: '4', title: 'Todo App', image: '', },
+      { id: '5', title: 'Accounting', image: '', },
+      { id: '6', title: 'Game', image: '', },
+      { id: '7', title: 'Book', image: '', },
+      { id: '8', title: 'Story', image: '', },
+    ]
   }
 
 }
