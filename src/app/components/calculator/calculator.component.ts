331aea73bf918b16e9091c840fe624678f3faecf
apip <apip@gitlab.lab>
2020-10-30 20:41:28 +0700

    calcultor function

--- src/app/components/calculator/calculator.component.ts
+++ src/app/components/calculator/calculator.component.ts
@@ -6,10 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calculator.component.css']
 })
 export class CalculatorComponent implements OnInit {
+  valueButtons: string[] = [];
+  previousOperand = '0';
+  currentOperand = '0';
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  getNumberValue(e: string) {
+    if (this.currentOperand !== '0') {
+      this.valueButtons = [this.currentOperand];
+      this.valueButtons.push(e);
+      this.currentOperand = this.valueButtons.join('');
+    } else {
+      this.valueButtons.push(e);
+      this.currentOperand = this.valueButtons.join('');
+    }
+  }
+
+  allClear() {
+    this.valueButtons = [];
+    this.previousOperand = '0';
+    this.currentOperand = '0';
+  }
+
+  deleteLastNum() {
+    this.currentOperand = this.currentOperand.slice(0, -1);
+  }
 }
