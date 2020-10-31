6c46fbe63c61fbf4a55689da1784b419be3e89f1
apip <apip@gitlab.lab>
2020-10-31 11:07:05 +0700

    styling card

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -14,14 +14,38 @@ export class DashboardComponent implements OnInit {
 
   ngOnInit(): void {
     this.itemCard = [
-      { id: 1, title: 'Covid Tracker', route: 'covid-tracker', image: '', deskripsi: '', postedTime: new Date() },
-      { id: 2, title: 'Calculator', route: 'calculator', image: '', deskripsi: '', postedTime: new Date() },
-      { id: 3, title: 'Timer', route: 'timer', image: '', deskripsi: '', postedTime: new Date() },
-      { id: 4, title: 'Todo App', route: 'todo-app', image: '', deskripsi: '', postedTime: new Date() },
-      { id: 5, title: 'Accounting', route: 'accounting', image: '', deskripsi: '', postedTime: new Date() },
-      { id: 6, title: 'Game', route: 'game', image: '', deskripsi: '', postedTime: new Date() },
-      { id: 7, title: 'Book', route: 'book', image: '', deskripsi: '', postedTime: new Date() },
-      { id: 8, title: 'Story', route: 'story', image: '', deskripsi: '', postedTime: new Date() }
+      {
+        id: 1, title: 'Covid Tracker', route: 'covid-tracker', image: '',
+        deskripsi: 'masih dalam pengerjaan', postedTime: new Date(), author: 'anynomous'
+      },
+      {
+        id: 2, title: 'Calculator', route: 'calculator', image: '',
+        deskripsi: 'simple calculator using angular', postedTime: new Date(), author: 'apip'
+      },
+      {
+        id: 3, title: 'Timer', route: 'timer', image: '',
+        deskripsi: 'masih dalam pengerjaan', postedTime: new Date(), author: 'anynomous'
+      },
+      {
+        id: 4, title: 'Todo App', route: 'todo-app', image: '',
+        deskripsi: 'masih dalam pengerjaan', postedTime: new Date(), author: 'anynomous'
+      },
+      {
+        id: 5, title: 'Accounting', route: 'accounting', image: '',
+        deskripsi: 'masih dalam pengerjaan', postedTime: new Date(), author: 'anynomous'
+      },
+      {
+        id: 6, title: 'Game', route: 'game', image: '',
+        deskripsi: 'masih dalam pengerjaan', postedTime: new Date(), author: 'anynomous'
+      },
+      {
+        id: 7, title: 'Book', route: 'book', image: '',
+        deskripsi: 'masih dalam pengerjaan', postedTime: new Date(), author: 'anynomous'
+      },
+      {
+        id: 8, title: 'Story', route: 'story', image: '',
+        deskripsi: 'masih dalam pengerjaan', postedTime: new Date(), author: 'anynomous'
+      }
     ]
   }
 
--- src/app/models/app-info.ts
+++ src/app/models/app-info.ts
@@ -5,4 +5,5 @@ export class AppInfo {
     image: string;
     deskripsi: string;
     postedTime: Date;
+    author: string;
 }
