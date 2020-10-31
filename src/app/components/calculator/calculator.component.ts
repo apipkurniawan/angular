5c00cb4b62088eb8d33b0a0439219b8451c4d12f
apip <apip@gitlab.lab>
2020-10-31 07:02:39 +0700

    create operation function

--- src/app/components/calculator/calculator.component.ts
+++ src/app/components/calculator/calculator.component.ts
@@ -9,6 +9,8 @@ export class CalculatorComponent implements OnInit {
   valueButtons: string[] = [];
   previousOperand = '0';
   currentOperand = '0';
+  resultOperand = '0';
+  operation: string;
 
   constructor() { }
 
@@ -25,12 +27,46 @@ export class CalculatorComponent implements OnInit {
     }
   }
 
-  allClear() {
-    this.valueButtons = [];
-    this.previousOperand = '0';
-    this.currentOperand = '0';
+  equalsBtn() {
+    this.compute();
+    this.clearCurrentOperand();
   }
 
+  operationBtn(e: string) {
+    this.operation = e;
+    if (this.previousOperand === '0') {
+      this.previousOperand = this.currentOperand;
+    }
+    this.clearCurrentOperand();
+  }
+
+  compute() {
+    switch (this.operation) {
+      case 'x':
+        this.previousOperand = (Number(this.currentOperand) * Number(this.previousOperand)).toString();
+        break;
+      case '÷':
+        this.previousOperand = (Number(this.previousOperand) / Number(this.currentOperand)).toString();
+        break;
+      case '+':
+        this.previousOperand = (Number(this.currentOperand) + Number(this.previousOperand)).toString();
+        break;
+      case '-':
+        this.previousOperand = (Number(this.previousOperand) - Number(this.currentOperand)).toString();
+        break;
+      default:
+        break;
+    }
+  }
+
+  clearCurrentOperand() {
+    this.valueButtons = [];
+    this.currentOperand = '0';
+  }
+  allClear() {
+    this.clearCurrentOperand();
+    this.previousOperand = '0';
+  }
   deleteLastNum() {
     this.currentOperand = this.currentOperand.slice(0, -1);
   }
